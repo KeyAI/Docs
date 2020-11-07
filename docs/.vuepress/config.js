1a233bc16805e0082148c38957e4d0f1bffcf8eb
KeyAI <mahongkaiwork@gmail.com>
2020-11-07 22:31:32 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: KeyAI
  * @Date: 2020-06-27 00:30:43
- * @LastEditTime: 2020-11-05 23:36:58
+ * @LastEditTime: 2020-11-07 22:28:27
  * @Description: 
  */
 module.exports = {
@@ -68,6 +68,7 @@ module.exports = {
                 {
                     title: '20-11',
                     children: [
+                        '/diaries/20-11/11-07',
                         '/diaries/20-11/11-05',
                         '/diaries/20-11/11-04',
                         '/diaries/20-11/11-02',
