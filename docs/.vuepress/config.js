bf543c1190a368a3cfd572b7bb93bc7a9058fd37
KeyAI <mahongkaichn@gmail.com>
2021-07-21 19:58:38 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,20 @@
-/*
- * @Author: KeyAI
- * @Date: 2020-06-27 00:30:43
- * @LastEditTime: 2020-12-07 19:27:11
- * @Description: 
- */
 module.exports = {
-    //基本配置
+    // 基本配置
     title: '浮槎游记',
     description: '就只是安静的，写一些东西，做一些努力',
     head: [
-        ['link', { rel: 'icon', href: '/img/favicon.ico' }], //添加 icon
-    ], 
-    base: '/docs/', //GitHub Pages 配置
+        ['link', { rel: 'icon', href: '/img/favicon.ico' }],
+    ],
+    // GitHub Pages 路由
+    base: '/docs/', 
     markdown: {
-        lineNumbers: true // 代码块显示行号
+        lineNumbers: true, // 代码块显示行号
     },
     //主题配置
     themeConfig: {
         repo: 'KeyAI/docs',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: 'GitHub',
 
         // 假如文档仓库和项目本身不在一个仓库：
@@ -33,38 +28,49 @@ module.exports = {
         // 默认为 "Edit this page"
         editLinkText: '源文档',
         nav: [
-          { text: '首页', link: '/' },
-          { text: '笔记', link: '/notes/' },
-          { text: '札册', link: '/books/' },
-          { text: '轻文档', link: '/lightdocs/' },
-          { text: '开发笔记', link: '/dev/' },
-          { text: '学习日记', link: '/diaries/' },
+            { text: '首页', link: '/' },
+            { text: '笔记', link: '/notes/' },
+            { text: '札册', link: '/books/' },
+            { text: '轻文档', link: '/lightdocs/' },
+            { text: '开发笔记', link: '/dev/' },
+            { text: '学习日记', link: '/diaries/' },
         ],
         sns: {
-            github: { 
-              account: 'KeyAI',
-              link: 'https://github.com/KeyAI'
+            github: {
+                account: 'KeyAI',
+                link: 'https://github.com/KeyAI'
             }
         },
         lastUpdated: '上次更新',
         sidebar: {
             '/notes/': [
-                ['','笔记'],
+                ['', '笔记'],
             ],
             '/books/': [
-                ['','札册'],
+                ['', '札册'],
             ],
             '/lightdocs/': [
-                ['','轻文档'],
-                ['vim','Vim'],
-                ['git','Git'],
-                ['VSCode','VSCode'],
+                ['', '轻文档'],
+                ['vim', 'Vim'],
+                ['git', 'Git'],
+                ['VSCode', 'VSCode'],
             ],
             '/dev/': [
-                ['','开发笔记'],
+                ['', '开发笔记'],
             ],
             '/diaries/': [
-                ['','学习日记'],
+                ['', '学习日记'],
+                {
+                    title: '21-07',
+                    children: [
+                        '/diaries/21-07/210721',
+                        '/diaries/21-07/210720',
+                        '/diaries/21-07/210719',
+                        '/diaries/21-07/210716',
+                        '/diaries/21-07/210708',
+                        '/diaries/21-07/210706',
+                    ]
+                },
                 {
                     title: '21-04',
                     children: [
@@ -164,12 +170,12 @@ module.exports = {
                 //         //         '/diaries/20-06/06-21',
                 //         //     ]
                 //         // },
-                       
+
                 //     ]
                 // },
-                
+
             ]
         },
     }
 
-  }
+}
